156d06dbb603d4fe8a534316b15fb4fa8382f60a
Georgina Torralba <georgina.torralba@gmail.com>
2021-07-17 00:57:40 +0200

    Add private Route

---
+++ src/components/auth/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,16 @@
+import { Redirect, Route, useLocation } from 'react-router-dom';
+
+import { getIsLogged } from '../../../store/selectors';
+import { useSelector } from 'react-redux';
+
+const PrivateRoute = (props) => {
+	const location = useLocation();
+	const isLogged = useSelector((state) => getIsLogged(state));
+	return isLogged ? (
+		<Route {...props} />
+	) : (
+		<Redirect to={{ pathname: '/login', state: { from: location } }} />
+	);
+};
+
+export default PrivateRoute;
