960a6e7944a3f1209fff5198cd3829808577cf95
Georgina Torralba <georgina.torralba@gmail.com>
2021-07-19 00:32:15 +0200

    Add function for image for detailAdvert

--- src/api/adverts.js
+++ src/api/adverts.js
@@ -3,13 +3,17 @@ import client from './client';
 export const BASE_URL = '/apiv1';
 
 export const getLatestAdverts = () => {
-  const url = `${BASE_URL}/advertisements`;
-  return client.get(url);
+	const url = `${BASE_URL}/advertisements`;
+	return client.get(url);
 };
 
+const mapAdvert = ({ image, ...advert }) => ({
+	...advert,
+	image: image
+		? `${process.env.REACT_APP_API_BASE_URL}images/adverts/${image}`
+		: image,
+});
 
-
-
-
-
-
+export const getAdvert = (advertId) => {
+	return client.get(`${BASE_URL}/${advertId}`).then(mapAdvert);
+};
