b0002a3c79173c09b184dbed477fa4b92af005ac
jaime Andres Triana <zubov2000@gmail.com>
2021-07-18 13:22:46 +0200

    delete: forgotPassword.js add: method forgotPassword in auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -4,24 +4,28 @@ import storage from '../utils/storage';
 const authPath = '/apiv1/auth';
 
 export const login = ({ remember, ...credentials }) => {
-	return client
-		.post(`${authPath}/signin`, credentials)
-		.then(({ token }) => {
-			configureClient({ token });
-			return token;
-		})
-		.then((token) => {
-			if (remember) {
-				storage.set('auth', token);
-			}
-		});
+  return client
+    .post(`${authPath}/signin`, credentials)
+    .then(({ token }) => {
+      configureClient({ token });
+      return token;
+    })
+    .then((token) => {
+      if (remember) {
+        storage.set('auth', token);
+      }
+    });
 };
 
 export const register = ({ ...credentials }) => {
-	console.log(`credentials en api/register.js ${credentials}`);
-	return client.post(`${authPath}/signup`, credentials);
+  console.log(`credentials en api/register.js ${credentials}`);
+  return client.post(`${authPath}/signup`, credentials);
+};
+
+export const forgotPassword = ({ ...credentials }) => {
+  return client.post(`${authPath}/forgot-password`, credentials);
 };
 
 export const logout = () => {
-	return Promise.resolve().then(resetClient).then(storage.clear);
+  return Promise.resolve().then(resetClient).then(storage.clear);
 };
