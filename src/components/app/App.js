9f6b436d4a2fa56467c7fbb504aa7abfe6410dc8
jaime Andres Triana <zubov2000@gmail.com>
2021-07-18 14:15:17 +0200

    add: forgotPassword in App.js add: forgotPassword and NewPassword in index.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,35 +1,42 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import { LoginPage, PrivateRoute, RegisterPage } from '../../components/auth';
+import {
+  LoginPage,
+  PrivateRoute,
+  RegisterPage,
+  ForgotPasswordPage,
+  NewPasswordPage,
+} from '../../components/auth';
 import { AdvertsPage } from '../Adverts';
 import NotfoundPage from './NotFoundPage';
 
 import './reset.css';
 import './App.css';
 
-
 function App() {
-	return (
-		<Switch>
-			<Route exact path="/login">
-				<LoginPage />
-			</Route>
+  return (
+    <Switch>
+      <Route exact path="/login">
+        <LoginPage />
+      </Route>
 
-			<Route exact path="/register">
-				<RegisterPage />
-			</Route>
+      <Route exact path="/register">
+        <RegisterPage />
+      </Route>
 
-			<Route exact path="/">
-				<AdvertsPage />
-			</Route>
-			<Route exact path="/404">
-				<NotfoundPage />
-			</Route>
-			<Redirect to="/404" />
-		</Switch>
-	);
+      <Route exact path="/Forgot-password">
+        <ForgotPasswordPage />
+      </Route>
+
+      <Route exact path="/">
+        <AdvertsPage />
+      </Route>
+      <Route exact path="/404">
+        <NotfoundPage />
+      </Route>
+      <Redirect to="/404" />
+    </Switch>
+  );
 }
-
-
 
 export default App;
--- src/components/auth/index.js
+++ src/components/auth/index.js
@@ -3,3 +3,5 @@ export { default as PrivateRoute } from './PrivateRoute';
 export { default as RegisterPage } from './RegisterPage';
 export { default as AuthButton } from './AuthButton';
 export { default as Header } from '../layout/Header';
+export { default as ForgotPasswordPage } from './ForgotPasswordPage';
+export { default as NewPasswordPage } from './NewPasswordPage';
