16713be76fbdc66fd72fab48e214d9fc3d29b540
Georgina Torralba <georgina.torralba@gmail.com>
2021-07-18 02:28:21 +0200

    Add AuthButton

--- src/components/auth/index.js
+++ src/components/auth/index.js
@@ -1,3 +1,4 @@
 export { default as LoginPage } from './LoginPage';
 export { default as PrivateRoute } from './PrivateRoute';
 export { default as RegisterPage } from './RegisterPage';
+export { default as AuthButton } from './AuthButton';
