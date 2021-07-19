1424ec61a70011d9b37421460f8fba8530fcd51b
Georgina Torralba <georgina.torralba@gmail.com>
2021-07-19 04:37:20 +0200

    Add route for advertDetail

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,46 +1,50 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import {
-  LoginPage,
-  PrivateRoute,
-  RegisterPage,
-  ForgotPasswordPage,
-  NewPasswordPage,
+	LoginPage,
+	PrivateRoute,
+	RegisterPage,
+	ForgotPasswordPage,
+	NewPasswordPage,
 } from '../../components/auth';
-import { AdvertsPage } from '../Adverts';
+import { AdvertsPage, AdvertPage } from '../Adverts';
 import NotfoundPage from './NotFoundPage';
 
 import './reset.css';
 import './App.css';
 
 function App() {
-  return (
-    <Switch>
-      <Route exact path="/login">
-        <LoginPage />
-      </Route>
+	return (
+		<Switch>
+			<Route exact path="/login">
+				<LoginPage />
+			</Route>
 
-      <Route exact path="/register">
-        <RegisterPage />
-      </Route>
+			<Route exact path="/register">
+				<RegisterPage />
+			</Route>
 
-      <Route exact path="/forgot-password">
-        <ForgotPasswordPage />
-      </Route>
+			<Route exact path="/forgot-password">
+				<ForgotPasswordPage />
+			</Route>
 
-      <Route exact path="/:new-password">
-        <NewPasswordPage />
-      </Route>
+			<Route exact path="/:new-password">
+				<NewPasswordPage />
+			</Route>
 
-      <Route exact path="/">
-        <AdvertsPage />
-      </Route>
-      <Route exact path="/404">
-        <NotfoundPage />
-      </Route>
-      <Redirect to="/404" />
-    </Switch>
-  );
+			<Route exact path="/adverts/:advertName/:advertId">
+				<AdvertPage />
+			</Route>
+
+			<Route exact path="/">
+				<AdvertsPage />
+			</Route>
+			<Route exact path="/404">
+				<NotfoundPage />
+			</Route>
+			<Redirect to="/404" />
+		</Switch>
+	);
 }
 
 export default App;
