8929ce5e3a07f7b7533b6b0eeb23e1f61c23ac11
Georgina Torralba <georgina.torralba@gmail.com>
2021-07-19 12:11:01 +0200

    Created SelectTags

---
+++ src/components/Adverts/SelectTags/SelectTags.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { tagsLoadedAction } from '../../../store/actions';
+import { getTags } from '../../../store/selectors';
+import { CheckboxGroup } from '../../shared';
+
+function SelectTags(props) {
+	const dispatch = useDispatch();
+	const tags = useSelector(getTags);
+
+	React.useEffect(() => {
+		dispatch(tagsLoadedAction());
+	}, []);
+
+	console.log('TAGS en selectTags', tags);
+	return <CheckboxGroup options={tags} {...props} />;
+}
+
+export default SelectTags;
