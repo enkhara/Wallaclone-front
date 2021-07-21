22610114b0c85591e7cc0d040e103419feaa42cb
jaime Andres Triana <zubov2000@gmail.com>
2021-07-21 20:15:51 +0200

    clean: commented code in forgotpasswordpage

--- src/components/auth/ForgotPasswordPage/ForgotPasswordPage.js
+++ src/components/auth/ForgotPasswordPage/ForgotPasswordPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import ForgotPasswordForm from './ForgotPasswordForm';
-import { forgotPassword } from '../../../api/auth';
 import { forgotPasswordAction } from '../../../store/actions';
 
 const ForgotPasswordPage = () => {
@@ -10,23 +9,10 @@ const ForgotPasswordPage = () => {
   const handleSubmit = async (credenciales) => {
     await dispatch(forgotPasswordAction(credenciales));
   };
-  // const [error, setError] = React.useState(null);
 
-  // const handleSubmit = async (credentials) => {
-  //   setError(null);
-  //   try {
-  //     await forgotPassword(credentials);
-  //     console.log('se envia el email para actualizar contraseña');
-  //   } catch (error) {
-  //     setError(error);
-  //   }
-
-  //   console.log(credentials);
-  // };
   return (
     <div>
       <ForgotPasswordForm onSubmit={handleSubmit} />
-      {/* {error && <div className="forgotPasswordPage-error">{error.message}</div>} */}
     </div>
   );
 };
