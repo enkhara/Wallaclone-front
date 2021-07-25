03f6c096559a40307b3a5b159dfcac36c63182b4
jaime Andres Triana <zubov2000@gmail.com>
2021-07-25 21:36:55 +0200

    fix: modification router newadverts

--- src/components/layout/Header/ButtonNewAdvert.js
+++ src/components/layout/Header/ButtonNewAdvert.js
@@ -4,22 +4,13 @@ import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import { useStyles } from '../../shared/useStyles';
 
 const ButtonNewAdvert = () => {
-    const classes = useStyles();
-    return (
-        <Link 
-            className={classes.containerNewAdvert}
-            to={`./adverts/new`} 
-        >
-            <AddCircleOutlineIcon className={classes.buttonNewAdvert}/>
-            Add new advert 
-           
-
-        </Link>
-      
-    );
+  const classes = useStyles();
+  return (
+    <Link className={classes.containerNewAdvert} to={`/adverts/new`}>
+      <AddCircleOutlineIcon className={classes.buttonNewAdvert} />
+      Add new advert
+    </Link>
+  );
 };
-
-
-
 
 export default ButtonNewAdvert;
