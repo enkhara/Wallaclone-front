3aa5326448637a37cbbb2217ed0275ab5d5b3d75
Georgina Torralba <georgina.torralba@gmail.com>
2021-07-26 00:59:05 +0200

    Add placheholder image

--- src/components/Adverts/AdvertsPage/Advert.js
+++ src/components/Adverts/AdvertsPage/Advert.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import T from 'prop-types';
 
+import placeholder from '../../../assets/images/placeholder.png';
+
 import { advert } from '../propTypes';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
@@ -13,32 +15,38 @@ import { useStyles } from '../../shared/useStyles';
 import ShareAdvert from '../../shareAdvert';
 import { Link } from 'react-router-dom';
 
-const Advert = ({ _id, image, name, desc, price, transaction, tags, reserved, sold}) => {
+const Advert = ({
+	_id,
+	image,
+	name,
+	desc,
+	price,
+	transaction,
+	tags,
+	reserved,
+	sold,
+}) => {
 	const classes = useStyles();
 	return (
 		<Grid item xs={12} sm={6} md={4}>
-			<Link 
-				to={`/adverts/${name}/${_id}`} 
-				style={{textDecoration:'none'}}>	
+			<Link to={`/adverts/${name}/${_id}`} style={{ textDecoration: 'none' }}>
 				<Card className={classes.card}>
 					<CardActionArea>
 						<CardMedia
 							className={classes.media}
-							image={`${process.env.REACT_APP_API_BASE_URL}/images/adverts/${image}`}
+							image={
+								image
+									? `${process.env.REACT_APP_API_BASE_URL}/images/adverts/${image}`
+									: placeholder
+							}
 						/>
 						<CardContent className={classes.cardContent}>
 							<Typography component="p" className={classes.priceAdvert}>
 								{`${price} €`}
 							</Typography>
-							<Typography component="p">
-								{name}
-							</Typography>
-							<Typography component="p">
-								{transaction}
-							</Typography>
-							<Typography component="p">
-								{desc}
-							</Typography>
+							<Typography component="p">{name}</Typography>
+							<Typography component="p">{transaction}</Typography>
+							<Typography component="p">{desc}</Typography>
 						</CardContent>
 					</CardActionArea>
 					<CardActions className={classes.cardActions}>
@@ -47,7 +55,6 @@ const Advert = ({ _id, image, name, desc, price, transaction, tags, reserved, so
 				</Card>
 			</Link>
 		</Grid>
-
 	);
 };
 
@@ -57,6 +64,5 @@ Advert.prototype = {
 	reserved: T.bool,
 	sell: T.bool,
 };
-
 
 export default Advert;
