dd9f26e49d9879ca4313ef15e49198d97a913859
Alicia Moltó <amoltovil@gmail.com>
2021-07-30 17:42:16 +0200

    add: Pagination

--- src/components/Adverts/AdvertsPage/AdvertsList.js
+++ src/components/Adverts/AdvertsPage/AdvertsList.js
@@ -4,23 +4,58 @@ import Advert from './Advert';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import { useStyles } from '../../shared/useStyles';
+//import { Box, List, Tag, ListItem, Divider } from "@chakra-ui/core";
+import { Pagination } from "@material-ui/lab";
+import usePagination from "../../hooks/usePagination";
 
+const AdvertsList = ({ adverts, count, page, pageSize }) => {
+	
+	const _advertsData = usePagination(adverts, pageSize);
 
-const AdvertsList = ({ adverts }) => {
+	 const handlePageChange = (event, value) => {
+	 	//setPage(value);
+	 	_advertsData.jump(value);
+	 };
+
+	// const handlePageSizeChange = (event) => {
+	// 	setPageSize(event.target.value);
+	// 	setPage(1);
+	//   };
 	
 	const classes = useStyles();
 	return (
 		<Container maxWidth="lg" className={classes.blogsContainer}>
+			<Pagination
+				count={count}
+				size="large"
+				page={page}
+				variant="outlined"
+				shape="rounded"
+				onChange={handlePageChange}
+			/>
 			<Grid container spacing={10}>
-				{adverts.map((advert) => (
+			 {/* <Box p="5">  */}
+			
+				{/* {adverts.map((advert) => (
+					<Advert key={advert._id} {...advert} />
+				))} */}
+				{_advertsData.currentData().map((advert) => (
 					<Advert key={advert._id} {...advert} />
 				))}
+				
+			 {/* </Box>	  */}
 			</Grid>
+			<Pagination
+					count={count}
+					size="large"
+					page={page}
+					variant="outlined"
+					shape="rounded"
+					onChange={handlePageChange}
+				/>
 		</Container>
 	);
 };
-
-
 
 
 AdvertsList.propTypes = {
--- src/components/Adverts/AdvertsPage/AdvertsPage.js
+++ src/components/Adverts/AdvertsPage/AdvertsPage.js
@@ -1,45 +1,121 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Header } from '../../layout';
 import { getAdverts } from '../../../store/selectors';
 import { advertsLoadAction } from '../../../store/actions';
 import EmptyList from './EmptyList';
 import AdvertsList from './AdvertsList';
-
-//<ShareAdvert Url={url_del_post} Titulo={titulo_del_post} Resumen={resumen_del_post} /> 
+import Pagination from "@material-ui/lab/Pagination";
 
 const AdvertsPage = () => {	
 	const dispatch = useDispatch();
 	const adverts = useSelector(getAdverts);
-	// paginación
-	const [limit, setLimit] = useState(9);
-    const [skip, setSkip] = useState(0);
 	
-	const nextPage = () => {
-		setSkip(skip + limit)
-	}
+	// paginación
+	// const [limit, setLimit] = useState(9);
+    // const [skip, setSkip] = useState(0);
+	
+	// const nextPage = () => {
+	// 	setSkip(skip + limit)
+	// }
 
-	const previousPage = () => {
-		setSkip(skip - limit)
-	}
+	// const previousPage = () => {
+	// 	setSkip(skip - limit)
+	// }
+	const [searchTitle, setSearchTitle] = useState("");
+	const [page, setPage] = useState(1);
+	//const [count, setCount] = useState(0);
+	const [pageSize, setPageSize] = useState(3);
 
-	React.useEffect(() => {
-		const filterinicial = ''; // traemos todos los anuncios del back
+	const pageSizes = [3, 6, 9, 12, 15, 18];
+	const count = Math.ceil(adverts.length / pageSize);
+
+	const onChangeSearchTitle = (e) => {
+		const searchTitle = e.target.value;
+		setSearchTitle(searchTitle);
+	};
+ 
+	const getRequestParams = (searchTitle, page, pageSize) => {
+		let params = {};
+	
+		if (searchTitle) {
+		  params["title"] = searchTitle;
+		}
+	
+		if (page) {
+		  params["page"] = page - 1;
+		}
+	
+		if (pageSize) {
+		  params["size"] = pageSize;
+		}
+	
+		return params;
+	  };
+
+	useEffect(() => {
+		//const filterinicial = ''; // traemos todos los anuncios del back
 		dispatch(advertsLoadAction()); //filterinicial, limit, skip));
-  	}, [dispatch]);
+		
+  	}, [dispatch, page, pageSize]);
+
+	const handlePageChange = (event, value) => {
+		setPage(value);
+	};
+
+	const handlePageSizeChange = (event) => {
+		setPageSize(event.target.value);
+		setPage(1);
+	  };
 
 	return(
 		<React.Fragment>			
-			<Header/>
+			<Header />
+			<div className="col-md-6">
+        		<h4>Adverts List</h4>
+				<div>Nº de páginas: {count} </div>
+				<div className="mt-3">
+					{"Adverts per Page: "}
+					<select onChange={handlePageSizeChange} value={pageSize}>
+						{pageSizes.map((size) => (
+						<option key={size} value={size}>
+							{size}
+						</option>
+						))}
+					</select>
+					
+					{/* <Pagination
+						className="my-3"
+						count={count}
+						page={page}
+						siblingCount={1}
+						boundaryCount={1}
+						variant="outlined"
+								shape="rounded"
+								color="primary"
+						onChange={handlePageChange}
+					/> */}
+        		</div>
+			</div>		
 				{adverts.length 
 				? 
 					<AdvertsList 
-						adverts={adverts}
+					adverts={adverts}
+					count={count}
+					page={page}
+					pageSize={pageSize}
 					/> 
 				: 	
 					<EmptyList/>
 				}
-				
+				{/* <Pagination
+					count={count}
+					size="large"
+					page={page}
+					variant="outlined"
+					shape="rounded"
+					onChange={handlePageChange}
+				/> */}
 		</React.Fragment>
 	)
 };
