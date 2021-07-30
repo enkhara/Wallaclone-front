1e423139eba036fff0e23e34a1e8c2e95c59d330
jaime Andres Triana <zubov2000@gmail.com>
2021-07-30 14:45:44 +0200

    add : translation in header

--- src/components/layout/Header/MenuLoginRegister.js
+++ src/components/layout/Header/MenuLoginRegister.js
@@ -1,25 +1,27 @@
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const MenuLoginRegister = () => {
-	return (
-		<div
-			style={{
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'center',
-			}}
-		>
-			<Link
-				to={`/login`}
-				style={{ color: '#fff', margin: '7px', textDecoration: 'none' }}
-			>
-				Login
-			</Link>
-			<Link to={`/register`} style={{ color: '#fff', textDecoration: 'none' }}>
-				Register
-			</Link>
-		</div>
-	);
+  const { t, i18n } = useTranslation(['global']);
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Link
+        to={`/login`}
+        style={{ color: '#fff', margin: '7px', textDecoration: 'none' }}
+      >
+        {t('header.Login')}
+      </Link>
+      <Link to={`/register`} style={{ color: '#fff', textDecoration: 'none' }}>
+        {t('header.Register')}
+      </Link>
+    </div>
+  );
 };
 
 export default MenuLoginRegister;
