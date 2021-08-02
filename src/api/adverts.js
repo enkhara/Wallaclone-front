d0942adccfbfbc2e6f2f0b1d46e7a5380c44d8dd
Georgina Torralba <georgina.torralba@gmail.com>
2021-08-03 01:24:44 +0200

    Cleaning code

--- src/api/adverts.js
+++ src/api/adverts.js
@@ -1,4 +1,4 @@
-import client, { configureClient, setAuthorizationHeader } from './client';
+import client from './client';
 
 export const BASE_URL = '/apiv1';
 
@@ -8,46 +8,34 @@ export const BASE_URL = '/apiv1';
 // };
 
 export const getLatestAdverts = () => {
-  const url = `${BASE_URL}/advertisements`;
-  return client.get(url);
+	const url = `${BASE_URL}/advertisements`;
+	return client.get(url);
 };
 
-const mapAdvert = ({ image, ...advert }) => ({
-  ...advert,
-  image: image
-    ? `${process.env.REACT_APP_API_BASE_URL}images/adverts/${image}`
-    : image,
-});
+// const mapAdvert = ({ image, ...advert }) => ({
+// 	...advert,
+// 	image: image
+// 		? `${process.env.REACT_APP_API_BASE_URL}images/adverts/${image}`
+// 		: image,
+// });
 
 export const getAllTags = () => {
-  return client.get(`${BASE_URL}/tags/allTags`);
+	return client.get(`${BASE_URL}/tags/allTags`);
 };
 
 export const getAdvert = (advertId) => {
-  const advert = client.get(`${BASE_URL}/advertisements/${advertId}`);
-  return advert;
-};
-
-export const getUSer = (userId) => {
-  const user = client.get(`${BASE_URL}/${userId}`);
-  console.log(user);
-  return user;
+	const advert = client.get(`${BASE_URL}/advertisements/${advertId}`);
+	return advert;
 };
 
 export const deleteAdvert = (advertId) => {
-  return client.delete(`${BASE_URL}/advertisements/${advertId}`);
+	return client.delete(`${BASE_URL}/advertisements/${advertId}`);
 };
 
 export const createdAdvert = (newAdvert) => {
-  return client.post(`${BASE_URL}/advertisements`, newAdvert);
-};
-const getToken = async function () {
-  return localStorage.getItem('token');
+	return client.post(`${BASE_URL}/advertisements`, newAdvert);
 };
 
 export const updateAdvert = (advertId, advert) => {
-  console.log('client', client);
-  //setAuthorizationHeader(token);
-  configureClient(getToken);
-  return client.put(`${BASE_URL}/advertisements/${advertId}`, advert);
+	return client.put(`${BASE_URL}/advertisements/${advertId}`, advert);
 };
