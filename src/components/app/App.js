34e91a5e5530d470c5657ce8ebc90a68109f10e0
Georgina Torralba <georgina.torralba@gmail.com>
2021-08-07 18:09:50 +0200

    Add routes for components ChatWithUser & UserWithUSer

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,58 +7,69 @@ import {
 	ForgotPasswordPage,
 	NewPasswordPage,
 } from '../../components/auth';
-import { AdvertsPage, AdvertPage, NewAdvertPage, EditAdvertPage } from '../Adverts';
-//import Chat from '../Chat/Chat';
+import {
+	AdvertsPage,
+	AdvertPage,
+	NewAdvertPage,
+	EditAdvertPage,
+} from '../Adverts';
+import UserPageWithUser from '../User/UserPage';
+import ChatWithUser from '../Chat/Chat';
 import NotfoundPage from './NotFoundPage';
 
 import './reset.css';
 import './App.css';
 
 function App() {
-  return (
-    <Switch>
-      <Route exact path="/login">
-        <LoginPage />
-      </Route>
+	return (
+		<Switch>
+			<Route exact path="/login">
+				<LoginPage />
+			</Route>
 
-      <Route exact path="/register">
-        <RegisterPage />
-      </Route>
+			<Route exact path="/register">
+				<RegisterPage />
+			</Route>
 
-      <Route exact path="/forgot-password">
-        <ForgotPasswordPage />
-      </Route>
+			<Route exact path="/forgot-password">
+				<ForgotPasswordPage />
+			</Route>
 
-      <Route exact path="/new-password/:id/:token">
-        <NewPasswordPage />
-      </Route>
+			<Route exact path="/new-password/:id/:token">
+				<NewPasswordPage />
+			</Route>
 
-      <Route exact path="/adverts/:advertName/:advertId">
-        <AdvertPage />
-      </Route>
+			<Route exact path="/adverts/:advertName/:advertId">
+				<AdvertPage />
+			</Route>
 
-      <PrivateRoute exact path="/adverts/new" component={NewAdvertPage} />
-      <PrivateRoute exact path="/adverts/edit/:advertId" component={EditAdvertPage} />
+			<PrivateRoute exact path="/adverts/new" component={NewAdvertPage} />
+			<PrivateRoute
+				exact
+				path="/adverts/edit/:advertId"
+				component={EditAdvertPage}
+			/>
 
-	   <Route exact path="/">
-        <Redirect to="/adverts" />
-		  </Route>
-		  
-	   {/* <PrivateRoute exact path="/chat/:advertId">
-			<Chat />
-		</PrivateRoute> */}
-		  
-      <Route exact path="/adverts">
-        <AdvertsPage />
-	    </Route>
-		  
-      <Route exact path="/404">
-        <NotfoundPage />
-      </Route>
-      <Redirect to="/404" />
-    </Switch>
-  );
+			<Route exact path="/">
+				<Redirect to="/adverts" />
+			</Route>
 
+			<PrivateRoute exact path="/user" component={UserPageWithUser} />
+
+			<PrivateRoute exact path="/chat/:advertId">
+				<ChatWithUser />
+			</PrivateRoute>
+
+			<Route exact path="/adverts">
+				<AdvertsPage />
+			</Route>
+
+			<Route exact path="/404">
+				<NotfoundPage />
+			</Route>
+			<Redirect to="/404" />
+		</Switch>
+	);
 }
 
 export default App;
