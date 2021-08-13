d843a4a18b15e18ac63a274708065f01d385e7ae
Jorge Salazar <j-salazarp@hotmail.com>
2021-08-13 09:34:33 +0200

    Depuramos el componente Logout para que el botón de salir de la sesión funcione correctamente.

--- src/components/layout/Header/Logout.js
+++ src/components/layout/Header/Logout.js
@@ -5,31 +5,39 @@ import Swal from 'sweetalert2';
 import { useTranslation } from 'react-i18next';
 
 const Logout = () => {
+
   const { t } = useTranslation(['global']);
+  const dispatch = useDispatch();
+  
+  const handleOpenModal = () => {
+    Swal.fire({
+      title: 'Are you sure?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const logoutSession = () => dispatch(authLogoutAction());
+        logoutSession();
+      }
+    });
+  };
 
-    const {t, i18n}=useTranslation(['global']);
-    const dispatch = useDispatch();
-
+   
   return (
-    <div style={{ marginTop: '3px' }}>
-      <Button color="inherit" onClick={handleOpenModal}>
-        {t('header.Logout')}
-      </Button>
-    </div>
+    <> 
+        <Button
+          variant='contained'
+          color='secondary'
+          onClick={handleOpenModal}
+        >
+          {t('header.Logout')}
+        </Button>
+    </>
   );
-};
-
-    return (
-        <> 
-            <Button
-                variant='contained'
-                color='secondary'
-                onClick={handleOpenModal}
-            >
-                {t('header.Logout')}
-            </Button>
-        </>
-    );
-
+}
 
 export default Logout;
+ 
