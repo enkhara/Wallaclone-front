3fdf259844b17052977dd6a736163ca7f7d5fb34
Georgina Torralba <georgina.torralba@gmail.com>
2021-08-13 11:38:16 +0200

    Add api/chat to store

--- src/store/store.js
+++ src/store/store.js
@@ -6,8 +6,9 @@ import reducer from './reducers/index';
 import * as auth from '../api/auth';
 import * as adverts from '../api/adverts';
 import * as user from '../api/user';
+import * as chat from '../api/chat';
 
-const api = { auth, adverts, user };
+const api = { auth, adverts, user, chat };
 
 const configureStore = ({ preloadedState, history }) => {
 	const middleware = [thunkMiddleware.withExtraArgument({ api, history })];
