ef71ed51ffdc73e5d150de470d6aac3b8bdf458e
jaime Andres Triana <zubov2000@gmail.com>
2021-08-14 14:04:38 +0200

    add:  translation registerPage.js  modal error

--- src/components/auth/RegisterPage/RegisterPage.js
+++ src/components/auth/RegisterPage/RegisterPage.js
@@ -5,25 +5,40 @@ import RegisterForm from './RegisterForm';
 import { registerAction, resetError } from '../../../store/actions';
 import { getUi } from '../../../store/selectors';
 import { Header } from '../../layout';
+import Swal from 'sweetalert2';
+import { useTranslation } from 'react-i18next';
 
 //** REDUX */
 function RegisterPage() {
-	const dispatch = useDispatch();
-	const { isLoading, error } = useSelector(getUi);
+  const dispatch = useDispatch();
+  const { isLoading, error } = useSelector(getUi);
+  const [t] = useTranslation('global');
 
-	const handleSubmit = async (credentials) => {
-		console.log(`credentials en RegisterPage ${credentials}`);
-		await dispatch(registerAction(credentials));
-	};
+  const handleSubmit = (credentials) => {
+    console.log(`credentials en RegisterPage ${credentials}`);
+    dispatch(registerAction(credentials));
+  };
 
-	return (
-		<div>
-			<Header />
-			<RegisterForm onSubmit={handleSubmit} />
-			{isLoading && <p>...registering in wallaclone</p>}
-			{error && <div onClick={() => dispatch(resetError())} />}
-		</div>
-	);
+  let status;
+  if (error != null) {
+    status = error.status;
+    if (status === 400) {
+      Swal.fire(
+        t('message.Somenthing goes wrong !'),
+        t('message.user already registered'),
+        t('message.Try again!')
+      );
+    }
+  }
+
+  return (
+    <div>
+      <Header />
+      <RegisterForm onSubmit={handleSubmit} />
+      {isLoading && <p>...registering in wallaclone</p>}
+      {error && <div onClick={() => dispatch(resetError())} />}
+    </div>
+  );
 }
 
 export default RegisterPage;
