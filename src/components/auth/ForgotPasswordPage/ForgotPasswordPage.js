21ca6866e2c84c8fa8f88ae5f1bde969e1bad5c0
jaime Andres Triana <zubov2000@gmail.com>
2021-08-14 14:00:22 +0200

    add: trnslation forgotpassword error

--- src/components/auth/ForgotPasswordPage/ForgotPasswordPage.js
+++ src/components/auth/ForgotPasswordPage/ForgotPasswordPage.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ForgotPasswordForm from './ForgotPasswordForm';
-import { forgotPasswordAction } from '../../../store/actions';
+import { forgotPasswordAction, resetError } from '../../../store/actions';
+import { getUi } from '../../../store/selectors';
+import Swal from 'sweetalert2';
+import { useTranslation } from 'react-i18next';
 
 const ForgotPasswordPage = () => {
   const dispatch = useDispatch();
+  const { isLoading, error } = useSelector(getUi);
+  const [t] = useTranslation('global');
 
   const handleSubmit = async (credenciales) => {
     await dispatch(forgotPasswordAction(credenciales));
+
+    let status;
+    if (error != null) {
+      status = error.status;
+      if (status === 400) {
+        Swal.fire(
+          t('message.Somenthing goes wrong !') + t('message.Unauthorized'),
+          t('message.Try again!')
+        );
+      }
+    }
   };
 
   return (
     <div>
       <ForgotPasswordForm onSubmit={handleSubmit} />
+      {error && <div onClick={() => dispatch(resetError())} />}
     </div>
   );
 };
