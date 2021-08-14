baf1754f64c941888d6b6e7d9ca9298ff9a8499a
jaime Andres Triana <zubov2000@gmail.com>
2021-08-14 14:01:27 +0200

    mod : translation loginpage.js  model error

--- src/components/auth/LoginPage/LoginPage.js
+++ src/components/auth/LoginPage/LoginPage.js
@@ -5,23 +5,37 @@ import LoginForm from './LoginForm';
 import { loginAction, resetError } from '../../../store/actions';
 import { getUi } from '../../../store/selectors';
 import { Header } from '../../layout';
+import Swal from 'sweetalert2';
+import { useTranslation } from 'react-i18next';
 
 function LoginPage() {
-	const dispatch = useDispatch();
-	const { isLoading, error } = useSelector(getUi);
+  const dispatch = useDispatch();
+  const { isLoading, error } = useSelector(getUi);
+  const [t] = useTranslation('global');
 
-	const handleSubmit = async (credentials) => {
-		await dispatch(loginAction(credentials));
-	};
+  const handleSubmit = (credentials) => {
+    dispatch(loginAction(credentials));
+  };
 
-	return (
-		<div>
-			<Header />
-			<LoginForm onSubmit={handleSubmit} />
-			{isLoading && <p>...login in wallaclone</p>}
-			{error && <div onClick={() => dispatch(resetError())} />}
-		</div>
-	);
+  let status;
+  if (error != null) {
+    status = error.status;
+    if (status === 401) {
+      Swal.fire(
+        t('message.Somenthing goes wrong ! Unauthorized'),
+        t('message.Try again!')
+      );
+    }
+  }
+
+  return (
+    <div>
+      <Header />
+      <LoginForm onSubmit={handleSubmit} />
+      {isLoading && <p>...login in wallaclone</p>}
+      {error && <div onClick={() => dispatch(resetError())} />}
+    </div>
+  );
 }
 
 export default LoginPage;
