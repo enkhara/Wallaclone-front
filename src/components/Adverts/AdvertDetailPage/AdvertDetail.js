1a03da8af803f50cb3d76dcfc26a80e9924f03a9
Georgina Torralba <georgina.torralba@gmail.com>
2021-08-14 17:42:00 +0200

    Fix bug with export AdvertDetailCSS again

--- src/components/Adverts/AdvertDetailPage/AdvertDetail.js
+++ src/components/Adverts/AdvertDetailPage/AdvertDetail.js
@@ -9,7 +9,7 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import CreateIcon from '@material-ui/icons/Create';
 import ChatIcon from '@material-ui/icons/Chat';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import { useStyles } from './advertDetailCSS';
+import { useStyles } from './AdvertDetailCSS';
 import ShareAdvert from '../shareAdvert';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
