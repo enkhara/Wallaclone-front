4b421bf4e29a9c495c8294c2002cad63fc29bb85
Jorge Salazar <j-salazarp@hotmail.com>
2021-08-21 16:21:33 +0200

    Incluide SelectTags and CSS in FiltersForm

--- src/components/Filters/FiltersForm.js
+++ src/components/Filters/FiltersForm.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import {
   Accordion,
@@ -8,61 +7,66 @@ import {
   TextField
 } from '@material-ui/core';
 import { SelectTags } from '../Adverts';
+import './FiltersForm.css';
 
 
-const AccordionDetailsContainer  = styled(AccordionDetails)`
-  width:100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-`;
-
-const InputText = styled.input`
-  width: 100%;
-  border: 2px solid lightblue;
-  border-radius:4px;
-  font-size:1.2rem;
-  outline:none;
-  padding: 0.8rem;
-  box-sizing:borderBox;
-  transition:.3s;
-  
-  &:focus{
-    border-color:dodgerBlue;
-    box-shadow:0 0 8px 0 dodgerBlue;
-  }
-`;
-
+ 
 const FiltersForm = () => {
-    return (
-      <section style={{margin:'2rem 0'}}>
-        <form>
-          <Accordion 
-            style={{width:'50%', margin:'0 auto'}}
-          >
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-          >
-                <h3 style={{margin:0}} >Search</h3>
-              </AccordionSummary>
-              <AccordionDetailsContainer>
+  
+  const [filter, setFilter] = React.useState({
+      name:'',
+      tags:[],
+  });
+
+  const { name, tags } = filter;
+
+  const handleChange = (event) => {
+    setFilter((oldAdvert) => ({
+      ...oldAdvert,
+      [event.target.name]:
+        event.target.type === 'checked'
+          ? event.target.checked
+          : event.target.value,
+    }));
+  };
+
+  return (
+    <section>
+      <form>
+        <Accordion 
+          style={{width:'50%', margin:'0 auto'}}
+        >
+          <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+        >
+            <h3>Search</h3>
+          </AccordionSummary>
+          <AccordionDetails
+            className="container-detail"
+          >            
+            <div>
+              <input
+                type="text"
+                placeholder="Name"
+                value={name}     
+                name="name"
+                onChange={handleChange}
+
+              />
+              <p>Tags</p>
+              <div 
+                className="tags"
+              >
+                <SelectTags
+                  multiple
+                  name="tags"
+                  value={tags}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
                 
-                  <div>
-                    <InputText
-                      type="text"
-                      placeholder="Name"
-                       
-                    />
-                    <p>Price</p>
-                    {/* <SelectTags
-                      multiple
-                      name="tags"
-                      value={tags}
-                      onChange={handleChange}
-                    /> */}
-                  </div>
-                
-            </AccordionDetailsContainer>
+            </AccordionDetails>
           </Accordion>
         </form>
       </section>
