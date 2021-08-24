02dffb93031686f90ed4997b6d74faab2efb36f2
Georgina Torralba <georgina.torralba@gmail.com>
2021-08-24 02:42:39 +0200

    Show username with conversation active

--- src/components/Chat/UserOwnAdvertChat.js
+++ src/components/Chat/UserOwnAdvertChat.js
@@ -1,14 +1,25 @@
 import './Chat.css';
 import Avatar from '@material-ui/core/Avatar';
+import { advertDetailAction } from '../../store/actions.js';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAdvertDetail } from '../../store/selectors.js';
+import React from 'react';
 
 const UserOwnAdvertChat = ({ currentChat }) => {
 	console.log('currentChat', currentChat);
+	const dispatch = useDispatch();
 
+	const advertId = currentChat.advertisementId._id;
+	const advert = useSelector((state) => getAdvertDetail(state, advertId));
+
+	React.useEffect(() => {
+		dispatch(advertDetailAction(advertId));
+	}, [dispatch, advertId]);
 	return (
 		<div className="userOwnBox">
 			<div className="userOwnInfo">
 				<Avatar className="UserOwnAdvertChatImg" src="" alt="" />
-				<p className="userOwnName">hola</p>
+				<p className="userOwnName">{advert?.userId.username}</p>
 			</div>
 		</div>
 	);
