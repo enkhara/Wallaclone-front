a99fbc656ed7eed609671e99055d26c73b1dc868
Georgina Torralba <georgina.torralba@gmail.com>
2021-08-30 02:24:39 +0200

    Add user image in Avatar

--- src/components/Chat/Message.js
+++ src/components/Chat/Message.js
@@ -3,11 +3,21 @@ import Avatar from '@material-ui/core/Avatar';
 
 import { format } from 'timeago.js';
 
-const Message = ({ message, own }) => {
+const Message = ({ message, own, currentChat, user }) => {
+	const speaker = currentChat.members.filter((c) => c._id !== user._id);
+
 	return (
 		<div className={own ? 'message own' : 'message'}>
 			<div className="messageTop">
-				<Avatar className="messageImg" src="" alt="" />
+				<Avatar
+					className="messageImg"
+					src={
+						own
+							? `${process.env.REACT_APP_API_BASE_URL}images/adverts/${user.image}`
+							: `${process.env.REACT_APP_API_BASE_URL}images/adverts/${speaker.image}`
+					}
+					alt=""
+				/>
 				<p className="messageText">{message.text}</p>
 			</div>
 			<div className="messageBottom">{format(message.createdAt)}</div>
