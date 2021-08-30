59216c2468c661a9a367cdc576123e29c76a0f2d
jaime Andres Triana <zubov2000@gmail.com>
2021-08-30 18:31:17 +0200

    fix: Warning: <Button> and <a>

--- src/components/Adverts/AdvertsPage/Advert.js
+++ src/components/Adverts/AdvertsPage/Advert.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import T from 'prop-types';
 import placeholder from '../../../assets/images/placeholder.png';
 import { advert } from '../propTypes';
@@ -8,203 +8,201 @@ import { NavLink } from 'react-router-dom';
 import { useStyles } from './advertCSS';
 
 import {
-	Grid,
-	Card,
-	CardActionArea,
-	CardActions,
-	CardMedia,
-	CardContent,
-	Typography,
-	Box,
-	Avatar,
-	IconButton,
+  Grid,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardMedia,
+  CardContent,
+  Typography,
+  Box,
+  Avatar,
+  IconButton,
 } from '@material-ui/core';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import classNames from 'classnames';
 //import { useHistory } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-	getUser,
-	getIsLogged,
-	getUserAdvertFavorite,
+  getUser,
+  getIsLogged,
+  getUserAdvertFavorite,
 } from '../../../store/selectors';
 import {
-	userAddFavoritesAction,
-	userDeleteFavoritesAction,
+  userAddFavoritesAction,
+  userDeleteFavoritesAction,
 } from '../../../store/actions';
 import { useTranslation } from 'react-i18next';
 const Advert = ({
-	_id,
-	image,
-	name,
-	desc,
-	price,
-	transaction,
-	tags,
-	reserved,
-	sold,
-	userId,
-	createdAt,
+  _id,
+  image,
+  name,
+  desc,
+  price,
+  transaction,
+  tags,
+  reserved,
+  sold,
+  userId,
+  createdAt,
 }) => {
-	const classes = useStyles();
-	const [t] = useTranslation('global');
-	//const history = useHistory();
-	const user = useSelector(getUser);
-	const isLogged = useSelector(getIsLogged);
-	const dispatch = useDispatch();
-	const [fav, setFav] = useState(false);
+  const classes = useStyles();
+  const [t] = useTranslation('global');
+  //const history = useHistory();
+  const user = useSelector(getUser);
+  const isLogged = useSelector(getIsLogged);
+  const dispatch = useDispatch();
+  const [fav, setFav] = useState(false);
 
-	//let fav = false;
-	// if (user) {
-	// 	fav = getUserAdvertFavorite(user, user._id, _id);
-	// }
+  //let fav = false;
+  // if (user) {
+  // 	fav = getUserAdvertFavorite(user, user._id, _id);
+  // }
 
-	// React.useEffect(() => {
-	//   if (user && user._id) {
-	//     getUserFav(user._id).then((r) => {
-	//       const favorites = r.result.ads_favs;
-	//       dispatch(setFavoritesUser(favorites));
-	//       setFav(favorites.includes(_id));
-	//     });
-	//   }
-	// }, [dispatch]);
+  // React.useEffect(() => {
+  //   if (user && user._id) {
+  //     getUserFav(user._id).then((r) => {
+  //       const favorites = r.result.ads_favs;
+  //       dispatch(setFavoritesUser(favorites));
+  //       setFav(favorites.includes(_id));
+  //     });
+  //   }
+  // }, [dispatch]);
 
-	// const handleFavored = async (e) => {
-	//   e.preventDefault();
-	//   setFav(!fav);
+  // const handleFavored = async (e) => {
+  //   e.preventDefault();
+  //   setFav(!fav);
 
-	//   if (!fav) {
-	//     await addFavorites(user._id, _id);
-	//     const favorites = user.ads_favs.push(_id);
-	//     dispatch(setFavoritesUser(favorites));
-	//   } else {
-	//     const favorites = user.ads_favs._id;
-	//     favorites && favorites.splice(favorites.indexOf(_id), 1);
-	//     await deleteFavorites(user._id, _id);
-	//   }
-	// };
+  //   if (!fav) {
+  //     await addFavorites(user._id, _id);
+  //     const favorites = user.ads_favs.push(_id);
+  //     dispatch(setFavoritesUser(favorites));
+  //   } else {
+  //     const favorites = user.ads_favs._id;
+  //     favorites && favorites.splice(favorites.indexOf(_id), 1);
+  //     await deleteFavorites(user._id, _id);
+  //   }
+  // };
 
-	useEffect(() => {
-		if (user) {
-			//console.log('estoy en el efecto')
-			setFav(getUserAdvertFavorite(user, user._id, _id));
-		}
-	}, [user, fav]);
+  useEffect(() => {
+    if (user) {
+      //console.log('estoy en el efecto')
+      setFav(getUserAdvertFavorite(user, user._id, _id));
+    }
+  }, [user, fav]);
 
-	const handleFavored = async (e) => {
-		e.preventDefault();
-		if (!fav) {
-			//console.log('añadimos favorito');
-			dispatch(userAddFavoritesAction(user._id, _id));
-		} else {
-			//console.log('borramos favorito');
-			dispatch(userDeleteFavoritesAction(user._id, _id));
-		}
-	};
+  const handleFavored = async (e) => {
+    e.preventDefault();
+    if (!fav) {
+      //console.log('añadimos favorito');
+      dispatch(userAddFavoritesAction(user._id, _id));
+    } else {
+      //console.log('borramos favorito');
+      dispatch(userDeleteFavoritesAction(user._id, _id));
+    }
+  };
 
+  return (
+    <Grid item xs={12} sm={6} lg={4} className={classes.containerGrid}>
+      <article className={classes.container_card}>
+        <NavLink
+          to={`/adverts/${name.replace(/\/+/g, '-')}/${_id}`}
+          style={{ textDecoration: 'none' }}
+        >
+          <Card className={classes.card}>
+            <CardActionArea>
+              <img
+                src={
+                  image
+                    ? `${process.env.REACT_APP_API_BASE_URL}images/adverts/${image}`
+                    : placeholder
+                }
+                alt="image advert"
+                className={classes.media}
+              />
 
-	return (
-		<Grid item xs={12} sm={6} lg={4} className={classes.containerGrid}>
-			<article className={classes.container_card}>
-				<NavLink
-					to={`/adverts/${name.replace(/\/+/g, '-')}/${_id}`}
-					style={{ textDecoration: 'none' }}
-				>
-					<Card className={classes.card}>
-						<CardActionArea>
-							<img
-								src={
-									image
-										? `${process.env.REACT_APP_API_BASE_URL}images/adverts/${image}`
-										: placeholder
-								}
-								alt="image advert"
-								className={classes.media}
-							/>
+              <CardContent className={classes.cardContent}>
+                <section className={classes.container_price_favorite}>
+                  {isLogged && (
+                    <FavoriteBorderIcon
+                      style={{ fontSize: '2rem' }}
+                      className={
+                        fav
+                          ? classNames(classes.favoriteIconSel)
+                          : classNames(classes.favoriteIcon)
+                      }
+                      onClick={handleFavored}
+                    />
+                  )}
 
-							<CardContent className={classes.cardContent}>
-								<section className={classes.container_price_favorite}>
-									{isLogged && (
-										<IconButton
-											className={
-												fav
-													? classNames(classes.favoriteIconSel)
-													: classNames(classes.favoriteIcon)
-											}
-											onClick={handleFavored}
-										>
-											<FavoriteBorderIcon style={{ fontSize: '2rem' }} />
-										</IconButton>
-									)}
+                  <p className={classes.priceAdvert}>{`${price} €`}</p>
+                </section>
 
-									<p className={classes.priceAdvert}>{`${price} €`}</p>
-								</section>
+                <h4 className={classes.cardNameAdvert}>{name}</h4>
+                <p className={classes.descAdvert}>{desc}</p>
+                <div className={classes.tagAndDescDetailAdvert}>
+                  <p>
+                    <span
+                      className={
+                        transaction === 'wanted'
+                          ? classNames(classes.wanted)
+                          : classNames(classes.sale)
+                      }
+                    >
+                      {transaction}
+                    </span>
+                  </p>
+                  <p>
+                    <span className={classes.spanDetailAdvert}>
+                      {tags.join(' - ')}
+                    </span>
+                  </p>
+                </div>
+              </CardContent>
+            </CardActionArea>
 
-								<h4 className={classes.cardNameAdvert}>{name}</h4>
-								<p className={classes.descAdvert}>{desc}</p>
-								<div className={classes.tagAndDescDetailAdvert}>
-									<p>
-										<span
-											className={
-												transaction === 'wanted'
-													? classNames(classes.wanted)
-													: classNames(classes.sale)
-											}
-										>
-											{transaction}
-										</span>
-									</p>
-									<p>
-										<span className={classes.spanDetailAdvert}>
-											{tags.join(' - ')}
-										</span>
-									</p>
-								</div>
-							</CardContent>
-						</CardActionArea>
+            <CardActions className={classes.cardActions}>
+              <NavLink
+                to={{
+                  pathname: `/${userId.username}/adverts`,
+                  state: { userId: `${userId._id}` },
+                }}
+                style={{ textDecoration: 'none' }}
+              >
+                <Box className={classes.author}>
+                  <Avatar />
 
-						<CardActions className={classes.cardActions}>
-							<NavLink
-								to={{
-									pathname: `/${userId.username}/adverts`,
-									state: { userId: `${userId._id}` },
-								}}
-								style={{ textDecoration: 'none' }}
-							>
-								<Box className={classes.author}>
-									<Avatar />
+                  <Box ml={2}>
+                    <Typography variant="subtitle2" component="p">
+                      {userId.username}
+                    </Typography>
 
-									<Box ml={2}>
-										<Typography variant="subtitle2" component="p">
-											{userId.username}
-										</Typography>
-
-										<Typography
-											variant="subtitle2"
-											color="textSecondary"
-											component="p"
-										>
-											{formatDistanceToNow(new Date(createdAt))}
-										</Typography>
-									</Box>
-								</Box>
-							</NavLink>
-							<ShareAdvert
-								Url={`${process.env.REACT_APP_FRONT_LOCALHOST}adverts/${name}/${_id}`}
-							/>
-						</CardActions>
-					</Card>
-				</NavLink>
-			</article>
-		</Grid>
-	);
+                    <Typography
+                      variant="subtitle2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      {formatDistanceToNow(new Date(createdAt))}
+                    </Typography>
+                  </Box>
+                </Box>
+              </NavLink>
+              <ShareAdvert
+                Url={`${process.env.REACT_APP_FRONT_LOCALHOST}adverts/${name}/${_id}`}
+              />
+            </CardActions>
+          </Card>
+        </NavLink>
+      </article>
+    </Grid>
+  );
 };
 
 Advert.prototype = {
-	...advert,
-	image: T.string,
-	reserved: T.bool,
-	sold: T.bool,
+  ...advert,
+  image: T.string,
+  reserved: T.bool,
+  sold: T.bool,
 };
 
 export default Advert;
