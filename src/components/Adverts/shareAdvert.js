cd0e611a035fb11472d82511d6b4c555bb2f15dc
Alicia Moltó <amoltovil@gmail.com>
2021-09-02 11:17:49 +0200

    fix: shareAdvert blank espaces

--- src/components/Adverts/shareAdvert.js
+++ src/components/Adverts/shareAdvert.js
@@ -14,18 +14,18 @@ class shareAdvert extends Component {
         return (
             <div>
                 <FacebookShareButton
-                    url={this.props.Url}
+                    url={this.props.Url.replace(/ /g, "%20")}
                     quote={`${this.props.Title} - ${this.props.Resumen}`}
                     hashtag="#"
                 >
                     <FacebookIcon round size={32} />
                 </FacebookShareButton>
 
-                <TwitterShareButton url={this.props.Url} title={this.props.Title}>
+                <TwitterShareButton url={this.props.Url.replace(/ /g, "%20")} title={this.props.Title}>
                     <TwitterIcon round size={32} style={{margin:'0 0.5rem'}}/>
                 </TwitterShareButton>
 
-                <WhatsappShareButton url={this.props.Url} title={this.props.title}>
+                <WhatsappShareButton url={this.props.Url.replace(/ /g, "%20")} title={this.props.title}>
                     <WhatsappIcon round size={32} />
                 </WhatsappShareButton>
 
